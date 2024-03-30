f70b0018bca6e496837c81546cfc92e98911d8f0
Karan0345 <karanvishwakarma0345@gmail.com>
2024-03-30 16:21:01 +0530

    changes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+
 import StreamVideoProvider from '@/providers/StreamClientProvider';
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
